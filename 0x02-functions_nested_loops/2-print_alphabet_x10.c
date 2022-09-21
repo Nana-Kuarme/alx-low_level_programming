3e616f5d96c495f235084244f523b3c6026cab91
Nana-Kuarme <kwammesh@yahoo.com>
2022-09-21 14:09:25 -0700

    t 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * prints_alphabets_10x - prints 10 times of the alhabets in lower case
+ *
+ * Return: Always 0
+ */
+void print_alphabet_10x(void)
+{
+	char i = 0, j;
+
+	for (i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
