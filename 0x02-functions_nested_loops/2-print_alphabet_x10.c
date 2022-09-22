f01f03342b42dea04506516f2970550085a79191
Nana-Kuarme <kwammesh@yahoo.com>
2022-09-22 10:11:10 -0700

    prints lowercase alphabet 10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,14 @@ void print_alphabet_x10(void)
 	char j;
 	int i = 0;
 
-	while (i = 10)
+	while (i < 10)
 	{
 		j = 'a';
 		while (j <= 'z')
 		{
 			_putchar(j);
 			j++;
-		}
+		} i++;
 		_putchar('\n');
 	}
 }
