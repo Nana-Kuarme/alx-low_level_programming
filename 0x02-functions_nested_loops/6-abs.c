acc2f7411dd8f99c8bbafa1f4b50332ee4a8bda9
Nana-Kuarme <kwammesh@yahoo.com>
2022-09-23 21:46:21 -0700

    absolute figur of n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - provides absolute function
+ *
+ * @n: integer togive absolute figure
+ *
+ * Return: int or - int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
