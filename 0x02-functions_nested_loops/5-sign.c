8ca1a266dd5cda471f3b4f8adb287e318ddc39c4
Nana-Kuarme <kwammesh@yahoo.com>
2022-09-23 21:27:36 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - main function
+ *
+ * @n: character to compare
+ *
+ * Return: 1 if +, 0 if zero, -1 if -
+ */
+
+int print_sign(int n)
+{
+	for (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	for (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	for (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
